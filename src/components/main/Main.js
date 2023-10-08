0a2de0fd351f51ce55e1f1b96372d90773fe8363
Vaneet Gupta <vaneet.gupta47@gmail.com>
2023-10-08 21:07:37 +0530

    sorting + pagination

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,6 +6,22 @@ import DialogBox from '../dialogBox/DialogBox';
 const apiUrl = 'https://mapi.harmoney.dev/api/v1/messages/';
 const headers = { Authorization: '0LZER559TVedxahu' };
 
+const handleSort = (order, currentItems) => {
+  let sortedItems;
+  if (order === 'oldest first') {
+    sortedItems = currentItems.sort(
+      (x, y) => new Date(x.timestamp) - new Date(y.timestamp)
+    );
+  } else {
+    sortedItems = currentItems.sort(
+      (x, y) => new Date(y.timestamp) - new Date(x.timestamp)
+    );
+  }
+  return sortedItems;
+};
+
+const messagesPerPage = 5;
+
 function Main() {
   const [inputVal, setInputVal] = useState('');
   const [messages, setMessages] = useState([]);
@@ -16,6 +32,11 @@ function Main() {
   const [popupSelected, setPopupSelected] = useState(false);
   const [loaderHidden, setLoaderHidden] = useState(true);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = (currentPage - 1) * messagesPerPage;
+  const endIndex = startIndex + messagesPerPage;
+  let currentItems = handleSort(order, messages).slice(startIndex, endIndex);
+
   const getMessages = async () => {
     setLoaderHidden(false);
     let res = await fetch(apiUrl, {
@@ -23,12 +44,7 @@ function Main() {
     });
     res = await res.json();
     setLoaderHidden(true);
-    if (order === 'oldest first') {
-      res = res.sort((x, y) => new Date(x.timestamp) - new Date(y.timestamp));
-    } else {
-      res = res.sort((x, y) => new Date(y.timestamp) - new Date(x.timestamp));
-    }
-
+    res = handleSort(order, res);
     setMessages(res);
   };
 
@@ -115,122 +131,150 @@ function Main() {
     }
   };
 
-  const handleSort = () => {
-    let newMsgs;
+  const changeSortOrder = () => {
     if (order === 'oldest first') {
-      newMsgs = messages.sort(
-        (x, y) => new Date(y.timestamp) - new Date(x.timestamp)
-      );
       setOrder('most recent first');
     } else {
-      newMsgs = messages.sort(
-        (x, y) => new Date(x.timestamp) - new Date(y.timestamp)
-      );
       setOrder('oldest first');
     }
-    setMessages(newMsgs);
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
-    <div className='main'>
-      <div className='cta-container'>
-        <input className='cta' value={inputVal} onChange={handleInputVal} />
-        <button
-          className='cta btn'
-          onClick={handlePost}
-          disabled={inputVal.trim().length === 0}
-        >
-          Post!
-        </button>
-        <button
-          className='cta btn delete'
-          onClick={() => {
-            let ids = messages.map((item) => item.id);
-            if (ids.length === 0) return;
-            setPopup(true);
-          }}
-          disabled={messages.length === 0}
-        >
-          Delete All
-        </button>
-        <button
-          className='cta btn delete'
-          onClick={() => {
-            let ids = messages.map((item) => item.id);
-            if (ids.length === 0) return;
-            setPopupSelected(true);
-          }}
-          disabled={checkedMsgs.length === 0}
-        >
-          Delete Selected
-        </button>
-        <button
-          className='cta btn sort'
-          onClick={() => {
-            handleSort();
-          }}
-          disabled={messages.length === 0}
-        >
-          {`Sort (currently ${order})`}
-        </button>
-      </div>
+    <>
+      <div className='main'>
+        <div className='cta-container'>
+          <input className='cta' value={inputVal} onChange={handleInputVal} />
+          <button
+            className='cta btn'
+            onClick={handlePost}
+            disabled={inputVal.trim().length === 0}
+          >
+            Post!
+          </button>
+          <button
+            className='cta btn delete'
+            onClick={() => {
+              let ids = messages.map((item) => item.id);
+              if (ids.length === 0) return;
+              setPopup(true);
+            }}
+            disabled={messages.length === 0}
+          >
+            Delete All
+          </button>
+          <button
+            className='cta btn delete'
+            onClick={() => {
+              let ids = messages.map((item) => item.id);
+              if (ids.length === 0) return;
+              setPopupSelected(true);
+            }}
+            disabled={checkedMsgs.length === 0}
+          >
+            Delete Selected
+          </button>
+          <button
+            className='cta btn sort'
+            onClick={() => {
+              changeSortOrder();
+            }}
+            disabled={messages.length === 0}
+          >
+            {`Change sort order? - currently ${order}`}
+          </button>
+        </div>
 
-      <div className='msg-conatiner'>
-        {messages.map((item, index) => {
-          return (
-            <div className='msg' key={index}>
-              <div className='msg-info'>
-                <input
-                  type='checkbox'
-                  id={index}
-                  onChange={(e) => {
-                    handleChecked(item.id, e);
-                  }}
-                  checked={checkedMsgs.includes(item.id)}
-                />
-                <span className='material-symbols-rounded icon'>sms</span>
-                <p className='src'>{item.source}</p>
-                &nbsp;
-                <p className='time'>-</p>
-                &nbsp;
-                <p className='time'>
-                  {moment(item.timestamp).format('hh:mm:ss a')}
-                </p>
-                <button
-                  className='delete-btn'
-                  onClick={() => {
-                    deleteMessage(item.id);
-                  }}
-                >
-                  Delete
-                </button>
+        <div className='msg-conatiner'>
+          {currentItems.map((item, index) => {
+            return (
+              <div className='msg' key={index}>
+                <div className='msg-info'>
+                  <input
+                    type='checkbox'
+                    id={index}
+                    onChange={(e) => {
+                      handleChecked(item.id, e);
+                    }}
+                    checked={checkedMsgs.includes(item.id)}
+                  />
+                  <span className='material-symbols-rounded icon'>sms</span>
+                  <p className='src'>{item.source}</p>
+                  &nbsp;
+                  <p className='time'>-</p>
+                  &nbsp;
+                  <p className='time'>
+                    {moment(item.timestamp).format('hh:mm:ss a')}
+                  </p>
+                  <button
+                    className='delete-btn'
+                    onClick={() => {
+                      deleteMessage(item.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+                <div className='msg-content'>{item.text}</div>
               </div>
-              <div className='msg-content'>{item.text}</div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
 
-      <div className='loader' hidden={loaderHidden}>
-        <img src='loader.svg' alt='loading' />
-      </div>
+        <div className='loader' hidden={loaderHidden}>
+          <img src='loader.svg' alt='loading' />
+        </div>
 
-      {popup && (
-        <DialogBox
-          closePopup={closePopup}
-          handleDeleteAll={handleDeleteAll}
-          message={'Are you sure to delete them all?'}
+        {popup && (
+          <DialogBox
+            closePopup={closePopup}
+            handleDeleteAll={handleDeleteAll}
+            message={'Are you sure to delete them all?'}
+          />
+        )}
+
+        {popupSelected && (
+          <DialogBox
+            closePopup={closePopupSelected}
+            handleDeleteAll={handleDeleteSelected}
+            message={'Are you sure to delete all the selected messages?'}
+          />
+        )}
+        
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil(messages.length / messagesPerPage)}
+          onPageChange={handlePageChange}
         />
-      )}
-      {popupSelected && (
-        <DialogBox
-          closePopup={closePopupSelected}
-          handleDeleteAll={handleDeleteSelected}
-          message={'Are you sure to delete all the selected messages?'}
-        />
-      )}
-    </div>
+      </div>
+    </>
   );
 }
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1
+  );
+
+  return (
+    <div className='pagination'>
+      {pageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          className={`${
+            pageNumber === currentPage ? 'active' : ''
+          } cta btn page-btn`}
+          onClick={() => onPageChange(pageNumber)}
+        >
+          {pageNumber}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default Main;
